95b1fe75f75f8731028fac3514093cbf68be7ab5
dinizdv <dinizdev3@gmail.com>
2024-04-06 18:53:47 -0300

    feat(home): responsive Login Page creation.

--- projects/login_database/src/pages/Home/index.js
+++ projects/login_database/src/pages/Home/index.js
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+import './home.css'
+import '../../index.css'
+
+import { Link } from 'react-router-dom'
+
 export default function Home(){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleLogin(e){
+        e.preventDefault(); /* does not refresh the page */
+        if (email !== '' && password !== ''){
+            alert('ok')
+        } else {
+            alert('Fill in the blank fields!')
+        }
+    }
+
+
     return (
-        <div>
-            <h1>Home page</h1>
+        <div className="home-container">
+            <h1>To-do List</h1>
+            <span>Manage your schedule easily.</span>
+        <form className="form" onSubmit={handleLogin}>
+            <input type="text" placeholder="your email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+            
+            <input type="password" autoComplete={false} placeholder="your password..." value={password} onChange={(e) => setPassword(e.target.value)} />
+        
+            <button type="submit">Access</button>
+        </form>
+
+        <Link className="button-link" to="/register">
+            Don´t have an account? Sign up!
+        </Link>
+
         </div>
     )
 }
