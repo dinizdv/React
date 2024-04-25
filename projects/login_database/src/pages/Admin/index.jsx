ba15b51cdcf4029fd1a56226f3e2955c35a2fcea
dinizdv <dinizdev3@gmail.com>
2024-04-25 19:59:14 -0300

    feat(Admin): styling the admin page and logout available.

--- projects/login_database/src/pages/Admin/index.jsx
+++ projects/login_database/src/pages/Admin/index.jsx
@@ -1,9 +1,41 @@
+import { useState } from 'react'
 import './admin.css'
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
 
 export default function Admin(){
+  const [taskInput, setTaskInput] = useState('')
+
+  function handleRegister(e){
+    e.preventDefault()
+    alert("Oi")
+  }
+
+  async function handleLogout(){
+    await signOut(auth)
+  }
+
   return(
-    <div>
-      <h1>Admin page</h1>
+    <div className="admin-container">
+      <h1>My tasks</h1>
+      <form className="form" onSubmit={handleRegister}>
+        <textarea placeholder="Type your task..." value={taskInput} onChange={(e) => setTaskInput(e.target.value)}/>
+
+        <button className="btn-register" type="submit">Register task</button>
+      </form>
+
+      <article className="list">
+        <p>Javascript and react today</p>
+
+        <div>
+          <button className="btn-edit">Edit task</button>
+          <button className="btn-delete">Complete task</button>
+        </div>
+      </article>
+      
+      <div className="container-btn">
+        <button className="btn-logout" onClick={handleLogout}>Logout</button>
+      </div>
     </div>
   )
 }
