7ca947d0ccf426172d571e3619e330ead895b901
dinizdv <dinizdev3@gmail.com>
2024-06-29 21:27:23 -0300

    feat(Signin): signin page styled.

--- projects/calling_system/src/pages/SignIn/index.jsx
+++ projects/calling_system/src/pages/SignIn/index.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import './signin.css'
+import { Link } from 'react-router-dom'
+
+
 export default function SignIn(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return(
-        <div>
-            <h1>Login page</h1>
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <i class="logo-login fa-regular fa-calendar-check"></i>
+                </div>   
+
+                <form>
+                    <h1>Login</h1>    
+                    <input type="text" placeholder='your_email@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                    <input type="password" placeholder='*********' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                    <button type="submit">Access</button>
+                </form> 
+
+                <Link to="/register">Create an account</Link>
+            </div>            
         </div>
     )
 }
