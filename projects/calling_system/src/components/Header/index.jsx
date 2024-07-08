7c950e77727dd81f6ec550d2a3647bbd5696f645
dinizdv <dinizdev3@gmail.com>
2024-07-08 11:44:30 -0300

    feat(Header): new header.

---
+++ projects/calling_system/src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react'
+import './header.css'
+import { Link } from 'react-router-dom'
+import { AuthContext } from '../../contexts/auth'
+import userAvatar from '../../assets/userAvatar.png'
+
+export default function Header(){
+    const { user } = useContext(AuthContext)
+    
+    return(
+        <div>
+            <div className="sidebar">
+                <div className='container-userAvatar'>
+                    <img className="userAvatar" src={user.avatarUrl === null ? userAvatar : user.avatarUrl} alt="User photo" />
+                </div>
+
+                <Link to='/dashboard'>
+                    <i className="icons-sidebar fa-solid fa-house"></i>calls
+                </Link>
+                <Link to='/customers'>
+                    <i className="icons-sidebar fa-solid fa-users"></i>clients
+                </Link>
+                <Link to='/profile'>
+                    <i className="icons-sidebar fa-solid fa-gear"></i>profile
+                </Link>
+            </div>
+        </div>
+    )
+}
--- projects/calling_system/src/pages/Dashboard/index.jsx
+++ projects/calling_system/src/pages/Dashboard/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
-
+import Header from "../../components/Header"
+import './dashboard.css'
 
 export default function Dashboard(){
     const { logout } = useContext(AuthContext)
@@ -11,6 +12,7 @@ export default function Dashboard(){
 
     return(
         <div>
+            <Header/>
             <h1>Dashboard page</h1>
             <button onClick={handleLogout}>Logout</button>
         </div>
