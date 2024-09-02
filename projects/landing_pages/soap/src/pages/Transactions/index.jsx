0e85de1a405f6c4c01467cb2cc717b191d557acf
dinizdv <dinizdev3@gmail.com>
2024-09-02 10:42:09 -0300

    feat(soap): adding and deleting infos in db.

--- projects/landing_pages/soap/src/pages/Transactions/index.jsx
+++ projects/landing_pages/soap/src/pages/Transactions/index.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
 import { db } from '../../services/firebaseConnection'
-import { getDocs, doc, collection } from 'firebase/firestore'
+import { getDocs, deleteDoc, doc, collection } from 'firebase/firestore'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import './transactions.css'
 
 
 export default function Transactions(){
     const [transactions, setTransactions] = useState([]);
     const price = 2.99
-    
+    var total = transactions.length * price
+
     useEffect(() => {
         fetchTransactions()
     }, [])
@@ -25,10 +27,24 @@ export default function Transactions(){
         } catch (error){
             console.log(error)
         }
-    }
-    
-    return(
+           
+}
 
+    // deleteDoc
+    async function deleteTransaction(id){
+        try {
+            const docRef = doc(db, 'purchases', id)
+            await deleteDoc(docRef)
+            // filter => create a new array with the elements that pass the thes implemented; it checks if the id of each transaction (t) does not match the id being deleted
+            setTransactions(prevTransactions => prevTransactions.filter(t => t.id !== id));
+            toast.success(`Transação do usuário ${transactions.find(t => t.id === id)?.name || 'indefinido'} deletada`);
+        } catch (error) {
+            toast.error(`Erro ao deletar transação do usuário ${transactions.name}`)
+            console.error("Erro ao deletar transação:", error)
+        }
+    }   
+
+    return(
                   <div className="container-dashboard">
                       <div className="container-style-dashboard">
                       <h1>Transações</h1>
@@ -47,6 +63,7 @@ export default function Transactions(){
                     <p className="purchase-name"><strong>E-mail:</strong>{transaction.email}</p>
                     <p className="purchase-name"><strong>N° telefone:</strong>{transaction.phone}</p>
                     <p className="purchase-name"><strong>Produto:</strong>{transaction.product}</p>
+                    <button onClick={() => deleteTransaction(transaction.id)}>Excluir</button>
                   </div>
         
                 ))
@@ -57,7 +74,7 @@ export default function Transactions(){
                           </div>
 
                           <p className='transactions-p'><strong>Total de transações:</strong> {transactions.length}</p>
-                          <p className='transactions-p'><strong>Valor arrecadado:</strong> R${transactions.length * price}</p>
+                          <p className='transactions-p'><strong>Valor arrecadado:</strong> R${total.toFixed(2)}</p>
                       </div>
                   </div>
     )
