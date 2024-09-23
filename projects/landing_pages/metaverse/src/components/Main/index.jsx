95527f986460beaa4bea92e5db6f48d9b0cef46e
dinizdv <dinizdev3@gmail.com>
2024-09-23 17:18:53 -0300

    feat: updated main.

--- projects/landing_pages/metaverse/src/components/Main/index.jsx
+++ projects/landing_pages/metaverse/src/components/Main/index.jsx
@@ -3,13 +3,25 @@ import Header from '../Header/index'
 import bgMain from '../../assets/bgMainnn.jpg'
 import bgMainn from '../../assets/bgMainn.jpg'
 import imgMain from '../../assets/metaverseMain.png'
+import { FaMicrosoft } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaSpotify } from "react-icons/fa";
+import { BsNvidia } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
+import { FaAmazon } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaMeta } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { SiFerrari } from "react-icons/si";
+import { SiMcdonalds } from "react-icons/si";
+
 
 const Main = () => {
     return (
         <div className="container-main">
             <Header/>
             <div className="container-bg-main">
-            <img src={bgMainn} alt="" />
+            <img src={bgMain} alt="" />
             </div>
 
 <div className="container-explore">
@@ -35,7 +47,7 @@ const Main = () => {
                         <label htmlFor="">seconds</label>
                     </div>
                 </div>
-                <a href="" id='buy-tickets-explore'>buy tickets</a>
+                <a href="" className='btn-main'>buy tickets</a>
             </section>
 
 
@@ -43,9 +55,68 @@ const Main = () => {
                 <img src={bgMainn} alt="" />
             </section>
 
-            section.
-
 </div>
+
+<section className="about-us">
+    <h1>About us</h1>
+</section>
+
+<section className="container-business">
+                    <h1>Listen to some of the biggest players in the Metaverse industry</h1>
+                    <div className="container-enterprises">
+                    <div className="enterprise">
+                        <label htmlFor="">microsoft</label>
+                        <FaMicrosoft className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">apple</label>
+                        <FaApple className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">x</label>
+                        <BsTwitterX className='logo-enterprise'/>
+                </div>               
+
+                <div className="enterprise">
+                        <label htmlFor="">spotify</label>
+                        <FaSpotify className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">amazon</label>
+                        <FaAmazon className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">google</label>
+                        <FaGoogle className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">meta</label>
+                        <FaMeta className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">instagram</label>
+                        <FaInstagram className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">ferrari</label>
+                        <SiFerrari className='logo-enterprise'/>
+                </div>
+
+                <div className="enterprise">
+                        <label htmlFor="">mcDonalds</label>
+                        <SiMcdonalds className='logo-enterprise'/>
+                </div>
+
+
+                </div>
+            </section>
 </div>
     )
 }
