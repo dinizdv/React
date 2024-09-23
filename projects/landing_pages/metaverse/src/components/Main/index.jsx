16eb2da13ec55d56a30727d10b922a79faa9689c
dinizdv <dinizdev3@gmail.com>
2024-09-23 14:29:44 -0300

    feat: main interface updated.

--- projects/landing_pages/metaverse/src/components/Main/index.jsx
+++ projects/landing_pages/metaverse/src/components/Main/index.jsx
@@ -1,17 +1,20 @@
 import '../../styles/main.css'
 import Header from '../Header/index'
 import bgMain from '../../assets/bgMainnn.jpg'
+import bgMainn from '../../assets/bgMainn.jpg'
+import imgMain from '../../assets/metaverseMain.png'
 
 const Main = () => {
     return (
         <div className="container-main">
             <Header/>
             <div className="container-bg-main">
-            <img src={bgMain} alt="" />
+            <img src={bgMainn} alt="" />
             </div>
 
-            <section className="explore">
-                <h1>explore the future of the metaverse</h1>
+<div className="container-explore">
+            <section className="explore-left">
+                <h1>explore the metaverse</h1>
                 <p>world´s largest online metaverse conference</p>
                 <p>2nd-3rd december 2024</p>
                 <div className="container-stopwatch">
@@ -21,20 +24,29 @@ const Main = () => {
                     </div>
                     <div className="stopwatch">
                         <label htmlFor="">63</label>
-                        <label htmlFor="">days</label>
+                        <label htmlFor="">hours</label>
                     </div>
                     <div className="stopwatch">
                         <label htmlFor="">63</label>
-                        <label htmlFor="">days</label>
+                        <label htmlFor="">minutes</label>
                     </div>
                     <div className="stopwatch">
                         <label htmlFor="">63</label>
-                        <label htmlFor="">days</label>
+                        <label htmlFor="">seconds</label>
                     </div>
                 </div>
                 <a href="" id='buy-tickets-explore'>buy tickets</a>
             </section>
-        </div>
+
+
+            <section className="explore">
+                <img src={bgMainn} alt="" />
+            </section>
+
+            section.
+
+</div>
+</div>
     )
 }
 
