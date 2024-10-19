db655a5082af29a64d81f57194ed8ee1dbc00c7b
dinizdv <dinizdev3@gmail.com>
2024-10-18 21:00:05 -0300

    feat(market): desktop Products.

--- projects/market/src/Components/Cart/index.jsx
+++ projects/market/src/Components/Cart/index.jsx
@@ -2,7 +2,45 @@ import '../../styles/cart.css'
 
 const Cart = () => {
     return (
-        <h1>cart</h1>
+        <div className="container-cart">
+            <h1>carrinho</h1>
+
+            <div className="container-product-cart">
+                <div className="product-cart">
+                    <label htmlFor=""><b>produto:</b>R$4,99</label>
+                    <label htmlFor=""><b>preço:</b>R$4,99</label>
+                    <label htmlFor=""><b>quantidade:</b>R$4,99</label>
+                </div>
+
+
+                <div className="product-cart">
+                    <label htmlFor=""><b>produto:</b>R$4,99</label>
+                    <label htmlFor=""><b>preço:</b>R$4,99</label>
+                    <label htmlFor=""><b>quantidade:</b>R$4,99</label>
+                </div>
+
+
+                <div className="product-cart">
+                    <label htmlFor=""><b>produto:</b>R$4,99</label>
+                    <label htmlFor=""><b>preço:</b>R$4,99</label>
+                    <label htmlFor=""><b>quantidade:</b>R$4,99</label>
+                </div>
+
+
+                <div className="product-cart">
+                    <label htmlFor=""><b>produto:</b>R$4,99</label>
+                    <label htmlFor=""><b>preço:</b>R$4,99</label>
+                    <label htmlFor=""><b>quantidade:</b>R$4,99</label>
+                </div>
+
+            </div> {/* container-product-cart */}
+
+            <div className="container-bills">
+            <label htmlFor=""><b>Total a pagar:</b>R$19,99</label>
+                    <label htmlFor=""><b>Dinheiro pago:</b><input type="text" placeholder='Insira o valor' /></label>
+                    <label htmlFor=""><b>Troco:</b>R$10.00</label>                
+            </div>
+        </div>
     )
 }
 
--- projects/market/src/Components/Products/index.jsx
+++ projects/market/src/Components/Products/index.jsx
@@ -1,33 +1,23 @@
 import '../../styles/products.css'
 import apple from '../../assets/apple.svg'
+import banana from '../../assets/banana.svg'
 
 const Products = () => {
     return (
         <main>
             <h1>mercadinho sESI </h1>
             <div className="container-products">
-                <div className="product">
-                    <img src={apple} alt="" />
+
+
+
+            <div className="product">
+                    <div className="container-img-product"><img src={apple} alt="" /></div>
                     <div className="container-info-product">
                         <label htmlFor=""><b>produto:</b>maçã</label>
                         <label htmlFor=""><b>preço:</b>R$4,99</label>
 
                         <select name="" id="">
-                            <option value="" disabled selected></option>
-                            <option value="">test</option>
-                        </select>
-                    </div>
-                </div>
-
-
-                <div className="product">
-                    <img src={apple} alt="" />
-                    <div className="container-info-product">
-                        <label htmlFor=""><b>produto:</b>maçã</label>
-                        <label htmlFor=""><b>preço:</b>R$4,99</label>
-
-                        <select name="" id="">
-                            <option value="" disabled selected></option>
+                            <option value="" disabled selected>Selecione a quantidade</option>
                             <option value="">test</option>
                         </select>
                     </div>
@@ -35,45 +25,68 @@ const Products = () => {
 
 
 
+
                 <div className="product">
-                    <img src={apple} alt="" />
+                    <div className="container-img-product"><img src={apple} alt="" /></div>
                     <div className="container-info-product">
                         <label htmlFor=""><b>produto:</b>maçã</label>
                         <label htmlFor=""><b>preço:</b>R$4,99</label>
 
                         <select name="" id="">
-                            <option value="" disabled selected>Quantidade</option>
+                            <option value="" disabled selected>Selecione a quantidade</option>
                             <option value="">test</option>
                         </select>
                     </div>
                 </div>
 
 
+
+
                 <div className="product">
-                    <img src={apple} alt="" />
+                    <div className="container-img-product"><img src={banana} alt="" /></div>
                     <div className="container-info-product">
                         <label htmlFor=""><b>produto:</b>maçã</label>
                         <label htmlFor=""><b>preço:</b>R$4,99</label>
 
                         <select name="" id="">
-                            <option value="" disabled selected></option>
+                            <option value="" disabled selected>Selecione a quantidade</option>
                             <option value="">test</option>
                         </select>
                     </div>
                 </div>
 
+
+
+
                 <div className="product">
-                    <img src={apple} alt="" />
+                    <div className="container-img-product"><img src={apple} alt="" /></div>
                     <div className="container-info-product">
                         <label htmlFor=""><b>produto:</b>maçã</label>
                         <label htmlFor=""><b>preço:</b>R$4,99</label>
 
                         <select name="" id="">
-                            <option value="" disabled selected></option>
+                            <option value="" disabled selected>Selecione a quantidade</option>
                             <option value="">test</option>
                         </select>
                     </div>
                 </div>
+
+
+
+                <div className="product">
+                    <div className="container-img-product"><img src={apple} alt="" /></div>
+                    <div className="container-info-product">
+                        <label htmlFor=""><b>produto:</b>maçã</label>
+                        <label htmlFor=""><b>preço:</b>R$4,99</label>
+
+                        <select name="" id="">
+                            <option value="" disabled selected>Selecione a quantidade</option>
+                            <option value="">test</option>
+                        </select>
+                    </div>
+                </div>
+
+
 
 
             </div> {/* container-products */}
