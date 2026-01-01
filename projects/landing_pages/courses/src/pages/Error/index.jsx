14e1414a2189ea961d65a3a01049b953fd711651
dinizdv <dinizdev3@gmail.com>
2026-01-01 11:18:45 -0300

    refactoring courses landing page.

--- projects/landing_pages/courses/src/pages/Error/index.jsx
+++ projects/landing_pages/courses/src/pages/Error/index.jsx
@@ -4,15 +4,15 @@ import './error.css'
 export default function Error(){
     return(
         <div className='bg'>
-        <div className="container-error">
-            <div className="container-elements">
-            <h1>404</h1>
-            <div className="container-p-and-link">
-            <p>Looks like we can't find this page! Return to the landing page to access resources.</p>
-            <Link to='/'>Access landing page</Link>
+            <div className="container-error">
+                <div className="container-elements">
+                    <h1>404</h1>
+                    <div className="container-p-and-link">
+                        <p>Looks like we can't find this page! Return to the landing page to access resources.</p>
+                        <Link to='/'>Access landing page</Link>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }
--- projects/landing_pages/courses/src/pages/Main/index.jsx
+++ projects/landing_pages/courses/src/pages/Main/index.jsx
@@ -1,80 +1,137 @@
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import Person from '../../assets/person.png'
 import BillGates from '../../assets/bill-gates.png'
 import JeffBezos from '../../assets/jeff-bezos.png'
-import Logo from '../../assets/logo.png'
 import ContactUs from '../../assets/contact.png'
 import BrunoDiniz from '../../assets/person-comment.png'
+
 import './main.css'
 
-export default function Main(){
-
-    function showSidebar(){
+export default function Main() {
+    function showSidebar() {
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'flex'
     }
 
-    function hideSidebar(){
+    function hideSidebar() {
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'none'
     }
 
+    return (
+        <div className="bg">
+            <header className="header">
+                <nav className="nav-header">
+                    <label className="logo">student guide</label>
 
+                    <ul className="sidebar">
+                        <li
+                            onClick={hideSidebar}
+                            className="li-nav-header"
+                        >
+                            <a href="#">
+                                <i className="fa-solid fa-x closeSidebar"></i>
+                            </a>
+                        </li>
 
-    return(
-        // header
-        <div className='bg'>
-            <header className='header'>
-                <nav className='nav-header'>
-                    <label className='logo'>student guide</label>
-                    {/* <label htmlFor="" className='logo'>Logo</label> */}
-                    <ul className='sidebar'>
-                        <li onClick={hideSidebar} className='li-nav-header'><a href="#"><i class="fa-solid fa-x closeSidebar"></i></a></li>
-                        <li className='li-nav-header sidebar-a'><a href="#">Home</a><i class="fa-solid fa-house"></i></li>
-                        <li className='li-nav-header sidebar-a'><a href="#">About</a><i class="fa-solid fa-comment"></i></li>
-                        <li className='li-nav-header sidebar-a'><a href="#">Courses</a><i class="fa-solid fa-book"></i></li>
-                        <li className='li-nav-header sidebar-a'><a href="#">Contact</a><i class="fa-solid fa-phone-flip"></i></li>
+                        <li className="li-nav-header sidebar-a">
+                            <a href="#">Home</a>
+                            <i className="fa-solid fa-house"></i>
+                        </li>
+
+                        <li className="li-nav-header sidebar-a">
+                            <a href="#">About</a>
+                            <i className="fa-solid fa-comment"></i>
+                        </li>
+
+                        <li className="li-nav-header sidebar-a">
+                            <a href="#">Courses</a>
+                            <i className="fa-solid fa-book"></i>
+                        </li>
+
+                        <li className="li-nav-header sidebar-a">
+                            <a href="#">Contact</a>
+                            <i className="fa-solid fa-phone-flip"></i>
+                        </li>
                     </ul>
-                    <ul className='ul-nav-header'>
-                        <li className='li-nav-header hideOnMobile'><a href="#">Home</a></li>
-                        <li className='li-nav-header hideOnMobile'><a href="#">About</a></li>
-                        <li className='li-nav-header hideOnMobile'><a href="#">Courses</a></li>
-                        <li className='li-nav-header hideOnMobile'><a href="#">Contact</a></li>
-                        <li onClick={showSidebar} className='menu-btn'><a href="#"><i class="fa-solid fa-bars"></i></a></li>
+
+                    <ul className="ul-nav-header">
+                        <li className="li-nav-header hideOnMobile">
+                            <a href="#">Home</a>
+                        </li>
+
+                        <li className="li-nav-header hideOnMobile">
+                            <a href="#">About</a>
+                        </li>
+
+                        <li className="li-nav-header hideOnMobile">
+                            <a href="#">Courses</a>
+                        </li>
+
+                        <li className="li-nav-header hideOnMobile">
+                            <a href="#">Contact</a>
+                        </li>
+
+                        <li
+                            onClick={showSidebar}
+                            className="menu-btn"
+                        >
+                            <a href="#">
+                                <i className="fa-solid fa-bars"></i>
+                            </a>
+                        </li>
                     </ul>
-                    {/* <button className='menu-collapse'>☰</button> */}
                 </nav>
             </header>
 
-{/* main */}
             <div className="container-section-main">
                 <section className="section-main-description">
-                    <h1 className='title-section-main-description'>Develop your skills in a unique way</h1>
-                    <p className='p-section-main-description'>Various courses on software development and programming! Discover all the courses below or watch a video about them</p>
+                    <h1 className="title-section-main-description">
+                        Develop your skills in a unique way
+                    </h1>
+
+                    <p className="p-section-main-description">
+                        Various courses on software development and programming!
+                        Discover all the courses below or watch a video about them
+                    </p>
+
                     <div className="container-btn-section-main">
-                        <button className='btn-main-section'>Access courses</button>
-                        <button className='btn-outline-main-section'>Watch video</button>
+                        <button className="btn-main-section">
+                            Access courses
+                        </button>
+
+                        <button className="btn-outline-main-section">
+                            Watch video
+                        </button>
                     </div>
                 </section>
 
                 <section className="section-main-image">
-                    <img src={Person} className='person' alt="" />
+                    <img
+                        src={Person}
+                        className="person"
+                        alt=""
+                    />
                 </section>
             </div>
 
-            {/* courses */}
             <section className="section-courses">
-                <h1 className='title-courses'>Courses</h1>
-                <p className='description-courses'>Below are our most popular courses and the total satisfaction percentage of students who completed them</p>
-                
-            <div className="container-circles">
+                <h1 className="title-courses">Courses</h1>
+
+                <p className="description-courses">
+                    Below are our most popular courses and the total satisfaction
+                    percentage of students who completed them
+                </p>
+
+                <div className="container-circles">
                     <div className="container-circle-elements">
                         <div className="circular-progress">
                             <div className="inner-circle"></div>
                             <p className="percentage">99%</p>
                         </div>
-                        <label htmlFor="" className='label-circle'>react JS + typeScript</label>
+
+                        <label className="label-circle">
+                            react JS + typeScript
+                        </label>
                     </div>
 
                     <div className="container-circle-elements">
@@ -82,7 +139,10 @@ export default function Main(){
                             <div className="inner-circle"></div>
                             <p className="percentage">96%</p>
                         </div>
-                        <label htmlFor="" className='label-circle'>advanced javaScript</label>
+
+                        <label className="label-circle">
+                            advanced javaScript
+                        </label>
                     </div>
 
                     <div className="container-circle-elements">
@@ -90,99 +150,159 @@ export default function Main(){
                             <div className="inner-circle"></div>
                             <p className="percentage">97%</p>
                         </div>
-                        <label htmlFor="" className='label-circle'>java for beginners</label>
+
+                        <label className="label-circle">
+                            java for beginners
+                        </label>
                     </div>
-            </div>
-                
+                </div>
             </section>
 
-{/* contact */}
             <div className="section-contact">
                 <section className="section-contact-img">
-                    <img src={ContactUs} className='contactUs-img' alt="" />
+                    <img
+                        src={ContactUs}
+                        className="contactUs-img"
+                        alt=""
+                    />
                 </section>
 
                 <section className="section-contact-form">
-                    <h1 className='title-section-contact-form'>get in touch</h1>
+                    <h1 className="title-section-contact-form">
+                        get in touch
+                    </h1>
+
                     <form action="">
                         <div className="container-input">
-                            <input type="text" className="input-form" placeholder='first name'/>
-                            <input type="text" className="input-form" placeholder='last name'/>
+                            <input
+                                type="text"
+                                className="input-form"
+                                placeholder="first name"
+                            />
+
+                            <input
+                                type="text"
+                                className="input-form"
+                                placeholder="last name"
+                            />
                         </div>
+
                         <div className="container-input">
-                            <input type="text" className="input-form" placeholder='email address'/>
-                            <input type="text" className="input-form" placeholder='phone number'/>
+                            <input
+                                type="text"
+                                className="input-form"
+                                placeholder="email address"
+                            />
+
+                            <input
+                                type="text"
+                                className="input-form"
+                                placeholder="phone number"
+                            />
                         </div>
-                        <textarea name="" id="" placeholder='Message (optional)'></textarea>
+
+                        <textarea
+                            placeholder="Message (optional)"
+                        ></textarea>
+
                         <div className="container-btnSend-form">
-                            <button className='btnSend-form'>Send</button>
+                            <button className="btnSend-form">
+                                Send
+                            </button>
                         </div>
                     </form>
                 </section>
             </div>
 
-            {/* recomendations */}
             <section className="section-comments">
-                <h1 className='comments-title'>Student´s Comments</h1>
-                <p className='comments-description'>Below are some comments from our students about their experiences with us</p>
+                <h1 className="comments-title">
+                    Student´s Comments
+                </h1>
+
+                <p className="comments-description">
+                    Below are some comments from our students about their experiences with us
+                </p>
+
                 <div className="container-comments">
-                 
-                <div className="comment">
-                    <div className="container-comment-person">
-                        <img src={BillGates} className='comment-img' alt="" />
-                        <div className="container-names-person">
-                            <p className="comment-name">bill gates</p>
-                            <p className="comment-job">fullstack developer</p>
-                        </div>
-                    </div>
-                    <div className="container-p-comment">
-                        <p className='p-comment'>I had an incredible experience with two courses taken here. I want to continue!</p>
-                    </div>
-                    </div>  
-
                     <div className="comment">
-                    <div className="container-comment-person">
-                        <img src={BrunoDiniz} className='comment-img' alt="" />
-                        <div className="container-names-person">
-                            <p className="comment-name">Bruno Diniz</p>
-                            <p className="comment-job">frontend developer</p>
+                        <div className="container-comment-person">
+                            <img
+                                src={BillGates}
+                                className="comment-img"
+                                alt=""
+                            />
+
+                            <div className="container-names-person">
+                                <p className="comment-name">bill gates</p>
+                                <p className="comment-job">fullstack developer</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="container-p-comment">
-                        <p className='p-comment'>It was a great opportunity to participate in the React JS + TypeScript course! I intend to take more courses.</p>
-                    </div>
+
+                        <p className="p-comment">
+                            I had an incredible experience with two courses taken here.
+                            I want to continue!
+                        </p>
                     </div>
 
                     <div className="comment">
-                    <div className="container-comment-person">
-                        <img src={JeffBezos} className='comment-img' alt="" />
-                        <div className="container-names-person">
-                            <p className="comment-name">jeff bezos</p>
-                            <p className="comment-job">backend developer</p>
+                        <div className="container-comment-person">
+                            <img
+                                src={BrunoDiniz}
+                                className="comment-img"
+                                alt=""
+                            />
+
+                            <div className="container-names-person">
+                                <p className="comment-name">Bruno Diniz</p>
+                                <p className="comment-job">fullstack developer</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="container-p-comment">
-                        <p className='p-comment'>I learned a lot about programming here and used my knowledge to create a system on Amazon!</p>
-                    </div>
+
+                        <p className="p-comment">
+                            It was a great opportunity to participate in the React JS + TypeScript course!
+                        </p>
                     </div>
 
+                    <div className="comment">
+                        <div className="container-comment-person">
+                            <img
+                                src={JeffBezos}
+                                className="comment-img"
+                                alt=""
+                            />
+
+                            <div className="container-names-person">
+                                <p className="comment-name">jeff bezos</p>
+                                <p className="comment-job">backend developer</p>
+                            </div>
+                        </div>
+
+                        <p className="p-comment">
+                            I learned a lot here. It helped me to create Amazon system!
+                        </p>
+                    </div>
                 </div>
             </section>
 
             <footer>
-                <label className='logo-footer'>student guide</label>
+                <label className="logo-footer">
+                    student guide
+                </label>
+
                 <div className="container-footer-elements">
                     <div className="container-footer-icons">
-                        <i class="fa-brands fa-linkedin-in"></i>
-                        <i class="fa-brands fa-facebook-f"></i>
-                        <i class="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-linkedin-in"></i>
+                        <i className="fa-brands fa-facebook-f"></i>
+                        <i className="fa-brands fa-instagram"></i>
                     </div>
+
                     <div className="container-copyright">
-                        <p className='p-copyright'>Copyright 2024. All Rights Reserved</p>
+                        <p className="p-copyright">
+                            Copyright 2024. All Rights Reserved
+                        </p>
                     </div>
                 </div>
             </footer>
-
-        </div> // bg 
+        </div>
     )
 }
